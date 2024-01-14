9ed692c360483a18b5f5d69b12a639e6d2e2702b
Amene Esnaashari <layla_1380@yahoo.com>
2024-01-13 22:42:48 -0600

    looping lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+//let names = ["Guadalupe ", "Ollie", "Aki"];
+
+function writeCards(arr,event){
+    const message = []
+    for(let i=0; i<arr.length; i++){
+        const note = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`
+        
+        message.push(note)
+    }
+    return message
+}
+
+
+
+console.log(writeCards(["Guadalupe ", "Ollie", "Aki"],"surprise"))
+
+function countDown(num){
+   
+    while(num >= 0){
+      console.log(num);
+      num--
+    }
+}
+
+   console.log(countDown(5))
+    
